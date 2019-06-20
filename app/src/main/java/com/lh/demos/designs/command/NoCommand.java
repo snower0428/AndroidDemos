07be1f311bac02fd408b7e41a21177d1ce0cba30
leihui <snower_tt@139.com>
2019-06-20 22:28:14 +0800

    update command

---
+++ app/src/main/java/com/lh/demos/designs/command/NoCommand.java
@@ -0,0 +1,14 @@
+package com.lh.demos.designs.command;
+
+/**
+ * Created by leihui on 2019/6/20.
+ * NoCommand
+ */
+
+public class NoCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ app/src/main/java/com/lh/demos/designs/command/RemoteControl.java
@@ -0,0 +1,46 @@
+package com.lh.demos.designs.command;
+
+/**
+ * Created by leihui on 2019/6/20.
+ * RemoteControl
+ */
+
+public class RemoteControl {
+
+    private Command[] mOnCommands;
+    private Command[] mOffCommands;
+
+    public RemoteControl() {
+        mOnCommands = new Command[7];
+        mOffCommands = new Command[7];
+
+        Command noCommand = new NoCommand();
+        for (int i = 0; i < 7; i++) {
+            mOnCommands[i] = noCommand;
+            mOffCommands[i] = noCommand;
+        }
+    }
+
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
+        mOnCommands[slot] = onCommand;
+        mOffCommands[slot] = offCommand;
+    }
+
+    public void onButtonWasPushed(int slot) {
+        mOnCommands[slot].execute();
+    }
+
+    public void offButtonWasPushed(int slot) {
+        mOffCommands[slot].execute();
+    }
+
+    public String toString() {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("\n---------- Remote Control ----------\n");
+        for (int i = 0; i < mOnCommands.length; i++) {
+            stringBuffer.append("[slot " + i + "] " + mOnCommands.getClass().getName()
+                    + "    " + mOffCommands.getClass().getName() + "\n");
+        }
+        return stringBuffer.toString();
+    }
+}
